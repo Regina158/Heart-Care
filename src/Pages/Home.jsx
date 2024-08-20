b4ba8267bf3485e2a77fc135bd32bec300081f01
regina158 <reginantx5@gmail.com>
2024-08-20 10:53:13 +0700

    First Commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 
 const Home = () => {
-  return (
-    <div className="bg-rose-600">
-      <Navbar/>
-    </div>
+    return (
+        <div>
+            <Navbar />
+            <div className="flex bg-purple-900 min-h-screen items-center justify-end">
+                <img
+                src="https://i.pinimg.com/originals/e3/18/d5/e318d52370cd99c920b0b05dd14c4ac3.jpg"
+                alt="Decorative"
+                className="w-auto h-auto max-w-md max-h-80 object-cover rounded-lg shadow-lg mr-20"
+                />
+            </div>
+        </div>
   );
 };
 

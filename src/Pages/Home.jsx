cc0f6f48ef484a5d09cdb45da3bf159fff492e18
regina158 <reginantx5@gmail.com>
2024-08-20 09:25:44 +0700

    first coomit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+
+const Home = () => {
+  return (
+    <div className="bg-rose-600">
+      <Navbar/>
+    </div>
+  );
+};
+
+export default Home;

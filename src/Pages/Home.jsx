92250d46637a9669601a8d9d0ab54eeddf0756fa
regina158 <reginantx5@gmail.com>
2024-08-20 11:09:08 +0700

    First Commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,17 +3,30 @@ import Navbar from "../Components/Navbar";
 
 const Home = () => {
     return (
-        <div>
-            <Navbar />
-            <div className="flex bg-purple-900 min-h-screen items-center justify-end">
-                <img
-                src="https://i.pinimg.com/originals/e3/18/d5/e318d52370cd99c920b0b05dd14c4ac3.jpg"
-                alt="Decorative"
-                className="w-auto h-auto max-w-md max-h-80 object-cover rounded-lg shadow-lg mr-20"
-                />
+      <div>
+        <Navbar />
+        <div
+          className="hero min-h-screen"
+          style={{
+            backgroundImage:
+              "url(https://i.pinimg.com/564x/21/7a/7a/217a7a60cc05cc990b8586fdec8076af.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
             </div>
+          </div>
         </div>
-  );
+      </div>
+    );
 };
 
 export default Home;
